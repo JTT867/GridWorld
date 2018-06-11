e816663e29c3173636072b47fe600507d6ca9ce7
JTT867 <39441942+JTT867@users.noreply.github.com>
2018-06-11 14:51:00 -0400

    Update QLearning.py
    reduce epsilon to stop the agent from exploring after several steps

--- QLearning.py
+++ QLearning.py
@@ -5,13 +5,13 @@ from collections import defaultdict
 class QLearning:
     def __init__(self, actions):
         self.alpha = 0.01    # the learning rate
-        self.gamma = 0.9    # the discount factot
+        self.gamma = 0.9    # the discount factor
         self.actions = actions
-        self.epsillon = 0.1    # use epsillon for epsillon greedy
+        self.epsilon = 0.1    # use epsilon for epsilon greedy
         self.q_table = defaultdict(lambda: [0.0, 0.0, 0.0, 0.0])
 
     def get_action(self, state):
-        if np.random.uniform() > self.epsillon:
+        if np.random.uniform() > self.epsilon:
             # choose best action according to q-table
             action_values = self.q_table[state]
             print("action_values", action_values)
@@ -19,13 +19,11 @@ class QLearning:
             for i in range(len(action_values)):
                 if action_values[i] == np.max(action_values):
                     argmax_actions.append(i)
-                    print(i)
             next_action = np.random.choice(argmax_actions)
-            print(argmax_actions)
-            print("next", next_action)
             # randomly choose an action from action base
         else:
             next_action = np.random.choice(self.actions)
+        self.epsilon -= 0.0005    # reduce epsilon to stop the agent from exploring after several steps
         return next_action
 
     def learn(self, current_state, current_action, reward, next_state):
@@ -35,5 +33,5 @@ class QLearning:
         self.q_table[current_state][current_action] = (1-self.alpha) * self.q_table[current_state][current_action] \
                                                       + self.alpha * new_q
         # current_q = self.q_table[current_state][current_action]
-        # # using Bellman Optimality Equation to update q function
+        # new_q = reward + self.gamma * max(self.q_table[next_state])
         # self.q_table[current_state][current_action] = (1-lr) * old_q + lr * new_q
